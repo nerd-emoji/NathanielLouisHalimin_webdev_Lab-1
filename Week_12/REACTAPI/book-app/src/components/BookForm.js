312eb53cfef59c767a8744e85fcab9ee20cd3ead
nerd-emoji <nathaniellouish@gmail.com>
2025-11-02 13:47:43 +0700

    update

--- Week_12/REACTAPI/book-app/src/components/BookForm.js
+++ Week_12/REACTAPI/book-app/src/components/BookForm.js
@@ -106,9 +106,9 @@ function BookForm({ onSubmit, bookToEdit, onCancel }) {
         <div className="mb-4">
           <label className="block mb-1 font-semibold">Rating</label>
           <select name="rating" value={formData.rating} onChange={handleChange} className="w-full px-3 py-2 border rounded">
-            <option value="excellent">Excellent</option>
-            <option value="average">Average</option>
-            <option value="bad">Bad</option>
+            <option value="excellent">⭐⭐⭐ Sangat Bagus</option>
+            <option value="average">⭐⭐ Biasa</option>
+            <option value="bad">⭐ Kurang Bagus</option>
           </select>
         </div>
 
